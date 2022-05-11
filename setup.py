fcb38c20a2155664699920637cbd6dc97d7df5df
AlexanderTeplyuk <aleksandrtepliuk@gmail.com>
2022-05-11 10:51:16 +0300

    first

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+def read(fname):
+    try:
+        with open(os.path.join(os.path.dirname(__file__), fname)) as fh:
+            return fh.read()
+    except IOError:
+        return ''
+
+requirements = read('requirements.txt').splitlines()
+
+setup(name='tst',
+      version='1.0.0',
+      description='test',
+      url='https://github.com/ateplyuk/ftbl',
+      packages=['ftbl'],
+      install_requires=requirements
+      )
